d88ef9624a5c50c34c97ba9d565b73946c9e14c4
pikopl <pikopl@users.noreply.github.com>
2018-01-26 09:19:00 +0100

    Added description for ShowerType enum

--- dbServiceWeather/src/main/java/com/xaltome/smartcity/weather/dbservice/model/ShowerType.java
+++ dbServiceWeather/src/main/java/com/xaltome/smartcity/weather/dbservice/model/ShowerType.java
@@ -9,23 +9,30 @@ package com.xaltome.smartcity.weather.dbservice.model;
  */
 public enum ShowerType {
 	
-	UNKNOWN(-1),
-	NO_SHOWER(69),
-	OCCASIONAL_RAIN(70),
-	CONSTANT_RAIN(71),
-	HEAVY_RAIN(72);
+	UNKNOWN(-1, "None"),
+	NO_SHOWER(69, "Brak opadu"),
+	OCCASIONAL_RAIN(70, "Opad przelotny"),
+	CONSTANT_RAIN(71, "Opad ciągły"),
+	HEAVY_RAIN(72, "Opad intensywny");
 	
 	private int code;
+	
+	private String description;
 
 	/**
 	 * @param value
 	 */
-	private ShowerType(int code) {
+	private ShowerType(int code, String description) {
 		this.code = code;
+		this.descripton = description;
 	}
 
 	public int getCode() {
 		return code;
+	}
+	
+	public String getDescription() {
+		return description;
 	}
 	
     public static ShowerType valueOf(int i){
@@ -35,5 +42,14 @@ public enum ShowerType {
     		}
     	}
     	throw new IllegalArgumentException("No matching constant for " + i);
+    }
+	
+    public static ShowerType valueOf(String desc){
+    	for (ShowerType showerType : values()){
+    		if (showerType.descripton.equals(desc)){
+    			return showerType;
+    		}
+    	}
+    	throw new IllegalArgumentException("No matching constant for " + desc);
     }
 }
